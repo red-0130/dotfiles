0a4207edf76fde592f88d7c7921634a1e30bb32b
Ephraim Beltran <100506096+ephraim-beltran@users.noreply.github.com>
2025-03-31 05:03:13 -0400

    fix(setup): fixed stow command syntax

--- configure.sh
+++ configure.sh
@@ -30,7 +30,7 @@ whiptail --title "Which config do you want to install?" --checklist --separate-o
     fi
 
     if [[ "sudo dpkg -s stow" ]]; then
-      stow -R "$SCRIPT_DIR/$choice"
+      stow -R -d "$SCRIPT_DIR/$choice"
     fi
 
     if [[ "$choice" -eq "fonts" ]]; then
