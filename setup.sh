197321a731f7fecd42a236b299a86764d06d3c9b
Ephraim Beltran <100506096+ephraim-beltran@users.noreply.github.com>
2025-03-31 05:57:27 -0400

    refactor(setup): remove the use of stow

--- setup.sh
+++ setup.sh
@@ -1,29 +1,23 @@
 #!/bin/env bash
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-if [[ "dpkg -s apt" ]]; then
-  sudo apt update
-fi
-if [[ "dpkg -s stow -eq 0" ]]; then
-  echo "Installing GNU Stow"
-  sudo apt install stow
-fi
 
 # Install Oh-My-Bash and import config
 echo "Installing Oh-My-Bash"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-stow -d "$SCRIPT_DIR" -R "bashrc"
+rm $HOME/.bashrc
+ln -s $SCRIPT_DIR/bashrc/.bashrc $HOME/.bashrc
 
 # Install latest Neovim release and import config
 echo "Installing Neovim"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-stow -d "$SCRIPT_DIR" -R "nvim"
+ln -s $SCRIPT_DIR/nvim/.config/nvim $HOME/.config/nvim
 
 # Install Superfile and import config
 echo "Installing Superfile"
 bash -c "$(curl -sLo- https://superfile.netlify.app/install.sh)"
-stow -d "$SCRIPT_DIR" -R "superfile"
+ln -s $SCRIPT_DIR/superfile/.config/superfile $HOME/.config/superfile
 
 echo "Script is finished executing"
