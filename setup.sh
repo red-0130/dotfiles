c8ad8b17c99e9a23a75d43bed17025389ac9af6f
Ephraim Beltran <100506096+ephraim-beltran@users.noreply.github.com>
2025-03-31 05:24:56 -0400

    fix(setup): clean up script

--- setup.sh
+++ setup.sh
@@ -12,30 +12,18 @@ fi
 # Install Oh-My-Bash and import config
 echo "Installing Oh-My-Bash"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-<<<<<<< HEAD
 stow -d "$SCRIPT_DIR -R bashrc"
-=======
-stow -R -d "$SCRIPT_DIR/bashrc"
->>>>>>> refs/remotes/origin/main
 
 # Install latest Neovim release and import config
 echo "Installing Neovim"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-<<<<<<< HEAD
 stow -d "$SCRIPT_DIR -R nvim"
-=======
-stow -R -d "$SCRIPT_DIR/nvim"
->>>>>>> refs/remotes/origin/main
 
 # Install Superfile and import config
 echo "Installing Superfile"
 bash -c "$(curl -sLo- https://superfile.netlify.app/install.sh)"
-<<<<<<< HEAD
 stow -d "$SCRIPT_DIR -R superfile"
-=======
-stow -R -d "$SCRIPT_DIR/superfile"
->>>>>>> refs/remotes/origin/main
 
 echo "Script is finished executing"
