56350ee2aa6a048200b9c1179e3315124cda9e4a
Ephraim Beltran <github@beltran.cc>
2025-04-23 08:38:06 +0000

    fix(nvim): fix install script

--- bin/install_nvim.sh
+++ bin/install_nvim.sh
@@ -33,13 +33,14 @@ installLazyvimDependencies() {
 }
 
 installLazygit() {
-  echo "Installing Lazygit" LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-  tar xf lazygit.tar.gz lazygit
+  printf "Installing Lazygit\n"
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*') &&
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" &&
+    tar xf lazygit.tar.gz lazygit
   sudo install lazygit -D -t /usr/local/bin/
-  echo "Finished installing. Beginning cleanup..."
+  printf "Finished installing. Beginning cleanup...\n"
   rm lazygit*
-  echo "Done."
+  printf "Done.\n"
 }
 
 installFzf() {
