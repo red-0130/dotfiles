52761e2ab7a0373002d693a659072cd1f959cbc0
Ephraim Beltran <100506096+ephraim-beltran@users.noreply.github.com>
2025-05-06 15:21:06 -0400

    refactor(setup.sh): removed installation of applicaitons in the script

--- setup.sh
+++ setup.sh
@@ -5,24 +5,24 @@ BIN="$ROOT_DIR/bin"
 
 nonInteractive() {
 
-  # Install Oh-My-Bash and import config
-  source "$BIN"/install_ohmybash.sh
+  # Apply bashrc config
+  source "$BIN"/bash_config.sh
 
   # Install latest Neovim release and import config
-  source "$BIN"/install_nvim.sh
+  source "$BIN"/config_nvim.sh
 
-  # Install Superfile and import config
-  source "$BIN"/install_spf.sh
+  # Apply Superfile config
+  source "$BIN"/config_spf.sh
 
   printf "Script is finished executing\n"
 
 }
 
 main() {
-  # Update APT repo
-  source "$BIN"/update_repo.sh
 
   if [[ "$1" == "-i" ]]; then
+    # Update APT repo
+    source "$BIN"/update_repo.sh
     source "$BIN/start_interactive.sh"
   else
     nonInteractive
