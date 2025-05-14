3ed032ecda138119f4657f67f39ad18cf0ef7594
Ephraim Beltran <github@beltran.cc>
2025-05-14 08:33:48 +0000

    fix(spf): spf isntall script now checks if bashrc file is a symlink

--- bin/config_spf.sh
+++ bin/config_spf.sh
@@ -2,7 +2,7 @@
 
 main() {
   echo "Applying Superfile config"
-  if ! grep "source "$HOME/.config/superfile/cd_on_quit.sh"" "$HOME/.bashrc"; then
+  if checkImports && checkIfLink ; then
     echo "Importing cd_on_quit script from config"
     echo -e "\n#Superfile cd_on_quit script" >>"$HOME/.bashrc"
     echo "source \""$HOME"/.config/superfile/cd_on_quit.sh\"" >>"$HOME/.bashrc"
@@ -10,6 +10,23 @@ main() {
   echo "Importing Superfile config..."
   source "$BIN/config_apply.sh" "superfile"
   echo "Done"
+}
+
+checkImports() {
+  if grep "source "$HOME/.config/superfile/cd_on_quit.sh"" "$HOME/.bashrc"; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+checkIfLink() {
+  if [[ -L "$HOME/.bashrc" ]]; then
+    echo "bashrc file is a symbolic link."
+    return 1 
+  else
+    return 0
+  fi
 }
 
 main "$@"
