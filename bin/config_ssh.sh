2319ba5fb5615f68ddc9b48b18e4179dd4f40e0e
Ephraim Beltran <100506096+ephraim-beltran@users.noreply.github.com>
2025-05-17 22:15:42 -0400

    refac(ssh): modify function chaining + use message constructor

--- bin/config_ssh.sh
+++ bin/config_ssh.sh
@@ -1,22 +1,17 @@
 #!/bin/env bash
 
 main() {
-  if checkConfigFile; then
-    applyConfig
-    appendIncludes
-  fi
-
-  if addScript; then
-    echo "SSH script added"
-  else
-    echo "There was an error adding the SSH script"
-  fi
+  checkConfigFile && applyConfig && appendIncludes && addScript
 }
 
 checkConfigFile() {
   local FILE="$HOME/.ssh/config"
   if [[ ! -e "$FILE" ]]; then
+    message warning SSH "Config file not found"
+    message info SSH "Creating config file now"
     touch "$FILE"
+  else
+    message info SSH "Config file found"
   fi
   return 0
 }
@@ -25,20 +20,32 @@ appendIncludes() {
   local FILE="$HOME/.ssh/config"
   local INCLUDES="Include "$HOME"/.config/ssh/config"
   if ! grep "$INCLUDES" "$FILE"; then
+    message info SSH "Appending config to config file"
     echo -e "$INCLUDES\n$(cat $FILE)" >"$FILE"
   fi
+  return 0
 }
 applyConfig() {
-  source "$BIN/config_apply.sh" "ssh"
+  message info SSH "Copying config files..."
+  if source "$BIN/config_apply.sh" "ssh"; then
+    message success SSH "Config file copy was succesful"
+    return 0
+  else
+    message error SSH "There was an error in copying config files"
+    return 1
+  fi
 }
 
 addScript() {
   if [[ ! -d "$HOME/.local/bin" ]]; then
+    message info SSH "User bin directory does not exist and will be created"
     mkdir -p "$HOME/.local/bin"
   fi
   if ln -sf "$ROOT_DIR/ssh/bin/onsubnet" "$HOME/.local/bin/onsubnet"; then
+    message success SSH "onsubnet script was successfully added"
     return 0
   else
+    message error SSH "There was an error adding onsubnet script"
     return 1
   fi
 }
