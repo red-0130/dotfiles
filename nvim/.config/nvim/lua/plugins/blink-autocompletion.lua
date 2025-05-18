2d9463f229da507b28f7f7d7333533f4a4748586
Ephraim Beltran <100506096+ephraim-beltran@users.noreply.github.com>
2025-05-18 03:13:42 -0400

    refac(nvim): removed blink.nvim keymap

--- nvim/.config/nvim/lua/plugins/blink-autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/blink-autocompletion.lua
@@ -1,11 +1,11 @@
-return {
-  "saghen/blink.cmp",
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = {
-      preset = "enter",
-      ["<Tab>"] = { "select_and_accept" },
-    },
-  },
-}
+-- return {
+--   "saghen/blink.cmp",
+--   ---@module 'blink.cmp'
+--   ---@type blink.cmp.Config
+--   opts = {
+--     keymap = {
+--       preset = "enter",
+--       ["<Tab>"] = { "select_and_accept" },
+--     },
+--   },
+-- }
