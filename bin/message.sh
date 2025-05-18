2f9e47d928eba0e694f3c9ec8ebf345ad9f6edb6
Ephraim Beltran <100506096+ephraim-beltran@users.noreply.github.com>
2025-05-17 23:18:48 -0400

    refac(message): modify application section styling

--- bin/message.sh
+++ bin/message.sh
@@ -6,23 +6,25 @@ message() {
   local BLUE='\e[34m'
   local RED='\e[31m'
   local YELLOW='\e[33m'
+  local BOLD='\e[39;1m'
+  local RESET='\e[0m'
   local TYPE="$1"
-  local APPLICATION="$BLUE[$2]$NOCOLOR"
+  local APPLICATION="$BOLD[$2]$RESET"
   local MESSAGE=$3
   local MESSAGE_TYPE=""
 
   case "$TYPE" in
   "success")
-    MESSAGE_TYPE="$GREEN[SUCCESS]$NOCOLOR"
+    MESSAGE_TYPE="$GREEN[SUCCESS]$RESET"
     ;;
   "error")
-    MESSAGE_TYPE="$RED[ERROR]$NOCOLOR"
+    MESSAGE_TYPE="$RED[ERROR]$RESET"
     ;;
   "warning")
-    MESSAGE_TYPE="$YELLOW[WARNING]$NOCOLOR"
+    MESSAGE_TYPE="$YELLOW[WARNING]$RESET"
     ;;
   *)
-    MESSAGE_TYPE="$NOCOLOR[INFO]"
+    MESSAGE_TYPE="$RESET[INFO]"
     ;;
   esac
 
