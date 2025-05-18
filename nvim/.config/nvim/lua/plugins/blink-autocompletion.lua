60885b357445dd6886a4b3e8d004dc7cbc1ed01a
Ephraim Beltran <100506096+ephraim-beltran@users.noreply.github.com>
2025-05-18 17:53:45 -0400

    fix(nvim): disable custom blink config

--- nvim/.config/nvim/lua/plugins/blink-autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/blink-autocompletion.lua
@@ -1,11 +1,14 @@
--- return {
---   "saghen/blink.cmp",
---   ---@module 'blink.cmp'
---   ---@type blink.cmp.Config
---   opts = {
---     keymap = {
---       preset = "enter",
---       ["<Tab>"] = { "select_and_accept" },
---     },
---   },
--- }
+if true then
+  return {}
+end
+return {
+  "saghen/blink.cmp",
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = "enter",
+      ["<Tab>"] = { "select_and_accept" },
+    },
+  },
+}
