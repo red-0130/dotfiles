38e1a1ab480c4fe07a8d57c7d3cc3dcbdd84387e
Ephraim Beltran <github@beltran.cc>
2025-06-21 07:19:24 +0000

    refactor(setup script): modify function scoping

--- setup.sh
+++ setup.sh
@@ -1,29 +1,30 @@
 #!/bin/env bash
 
-nonInteractive() {
-
-  # Apply bashrc config
-  source "$BIN"/bash_config.sh
-  bash_config
-
-  # Install latest Neovim release and import config
-  source "$BIN"/config_nvim.sh
-
-  # Apply Superfile config
-  source "$BIN"/config_spf.sh
-
-  # Apply SSH config
-  source "$BIN"/config_ssh.sh
-
-  printf "Script is finished executing\n"
-
-}
-
 main() {
   local ROOT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
   local BIN="$ROOT_DIR/bin"
   source "$BIN/message.sh"
   source "$BIN/shell_logger.sh"
+
+  nonInteractive() {
+    log_info setup "Starting non-interactive setup script."
+
+    # Apply bashrc config
+    source "$BIN"/bash_config.sh
+    bash_config
+
+    # Install latest Neovim release and import config
+    source "$BIN"/config_nvim.sh
+
+    # Apply Superfile config
+    source "$BIN"/config_spf.sh
+
+    # Apply SSH config
+    source "$BIN"/config_ssh.sh
+
+    log_success setup "Script is finished executing"
+
+  }
 
   if [[ "$1" == "-i" ]]; then
     # Update APT repo
