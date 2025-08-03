1063e39360308b4b7fa4f0807a2daad2eb7c425a
Ephraim Beltran <github@beltran.cc>
2025-08-03 23:27:44 +0000

    refactor: cleanup for v2

--- bashrc/.config/bashrc/paths.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-
-# ADD CUSTOM PATHS HERE
-source $HOME/.config/bashrc/lib/create-component.sh
-source $HOME/.config/bashrc/lib/workspacectl.sh
-source $HOME/.config/bashrc/lib/onsubnet.sh
--- bin/install_eza.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-if command -v eza &>/dev/null; then
-  echo "eza is already installed"
-else
-  sudo apt install eza -y
-fi
