6306876cb87ed634bf65d1557c9ee1434768b006
Ephraim Beltran <github@beltran.cc>
2025-08-07 04:43:42 -0400

    fix(mpv config): apply config for flatpak installations

--- config/mpv/install_config.sh
+++ config/mpv/install_config.sh
@@ -3,6 +3,11 @@ if ! source "$BIN/path_exist.sh"; then exit 1; fi
 
 main() {
   log_info mpv "Applying config..."
+  local CONFIG_FLATPAK="$HOME/.var/app/io.mpv.Mpv/config/"
+  if [[ -d "$CONFIG_FLATPAK" ]]; then
+    log_info mpv "Flatpak installation detected. Copying to Flatpak app."
+    ln -sf "$CONFIG/mpv/config/mpv" "$HOME/.var/app/io.mpv.Mpv/config/"
+  fi
   if copy_config mpv; then
     log_success mpv "Successfully applied config."
   fi
