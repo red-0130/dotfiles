096157d82e81c549a34da127cf63d1e4c43e5546
Ephraim Beltran <github@beltran.cc>
2025-08-28 03:07:12 -0400

    refactor(neovim install): Lazyvim dependencies is now installed from source

--- install/nvim.sh
+++ install/nvim.sh
@@ -5,36 +5,71 @@ if ! source "$BIN/path_exist.sh"; then exit 1; fi
 main() {
 
   installFzf() {
-    log_info fzf "Installing fzf"
-    if sudo apt install fzf -y; then
-      log_success fzf "Fzf installed successfully."
+    log_info fzf "Installing fzf..."
+    log_info fzf "Downloading install files from repo..."
+    if git clone --depth 1 https://github.com/junegunn/fzf.git /tmp/fzf; then
+      log_info fzf "Files downloaded /tmp/fzf. Starting installation now."
+    else
+      log_error fzf "Install files could not be downloaded."
+      return 1
+    fi
+
+    if /tmp/fzf/install --all; then
+      log_success fzf "Successfully installed."
       return 0
     else
-      log_error fzf "There was an error installing Fzf."
-      exit 1
+      log_error fzf "There was an error installing."
+      return 1
     fi
   }
 
   installFd() {
     log_info "fd-find" "Installing fd-find"
-    if sudo apt install fd-find -y; then
-      log_success "fd-find" "Successfully installed fs-find."
-      return 0
+
+    local FD_VERSION=$(curl -sL "https://api.github.com/repose/sharkdp/fd/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
+
+    log_info "fd-find" "Downloading install files from repo..."
+    if curl --create-dirs \
+      -sLo /tmp/fd/fdfind.deb \
+      "https://github.com/sharkdp/fd/releases/download/v${FD_VERSION}/fd-musl_${FD_VERSION}_amd64.deb"; then
+      log_info "fd-find" "Files downloaded. Starting installation..."
     else
-      log_error "fd-find" "There was an error installing fd-find."
+      log_error "fd-find" "Install files could not be downloaded."
       return 1
     fi
+
+    if sudo dpkg -i /tmp/fd/fdfind.deb; then
+      log_success "fd-find" "Successfully installed."
+      return 0
+    else
+      log_error "fd-find" "There was an error installing."
+      return 1
+    fi
+
   }
   installRipgrep() {
     log_info ripgrep "Installing ripgrep"
-    if sudo apt install ripgrep -y; then
-      log_success ripgrep "Ripgrep successfully installed."
+    local RG_VERSION=$(curl -sL "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep "tag_name" | cut -d '"' -f 4)
+
+    log_info ripgrep "Downloading install files from repo..."
+    if curl --create-dirs \
+      -sLo /tmp/ripgrep/ripgrep.deb \
+      -sLo /tmp/dependencies/ripgrep.deb "https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep_${RIPGREP_VERSION}-1_amd64.deb"; then
+      log_info ripgrep "Files downloaded. Starting installation..."
+    else
+      log_error ripgrep "Install files could not be downloaded."
+      return 1
+    fi
+
+    if sudo dpkg -i /tmp/ripgrep/ripgrep.deb; then
+      log_success ripgrep "Successfully installed."
       return 0
     else
-      log_error ripgrep "There was an error installing Ripgrep."
+      log_error ripgrep "There was an error installing."
       return 1
     fi
   }
+
   installLazygit() {
     log_info lazygit "Starting installation..."
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*') &&
@@ -44,7 +79,7 @@ main() {
       log_success lazygit "Finished installing. Beginning cleanup..."
     else
       log_error lazygit "There was an error installing Lazygit"
-      exit 1
+      return 1
     fi
     if rm lazygit*; then
       log_success lazygit "Done"
@@ -54,6 +89,7 @@ main() {
       return 0
     fi
   }
+
   installNeovim() {
     log_info nvim "Installing Neovim"
     if ! command nvim -v &>/dev/null; then
@@ -74,4 +110,5 @@ main() {
   log_info nvim "Starting installation..."
   installFzf && installFd && installRipgrep && installLazygit && installNeovim && return 0
 }
+
 if main; then log_success nvim "Neovim is done installing."; fi
