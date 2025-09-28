c78c40b01ed27c087f1b11ce8744434056f4968d
Ephraim Beltran <github@beltran.cc>
2025-09-28 05:04:30 -0400

    fix(nvim): fix colorscheme loading

--- config/nvim/config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/config/nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,9 @@
 return {
   { "catppuccin/nvim", lazy = true, name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
 }
