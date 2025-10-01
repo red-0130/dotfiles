ca4e01b273c969c731ba5b3c83a2ba503746fb6d
Ephraim Beltran <github@beltran.cc>
2025-10-01 00:53:29 -0400

    feature(mkcomp): add flag parsing for component redirection

--- config/bashrc/config/bashrc/lib/create-component.sh
+++ config/bashrc/config/bashrc/lib/create-component.sh
@@ -1,6 +1,7 @@
 mkcomp() (
   local COMPONENT_NAME="$1"
   local CWD=$(pwd)
+  local SRC="$CWD/src"
   local COMPONENT_DIR="$CWD/src/$COMPONENT_NAME"
   local INDEXFILE="$COMPONENT_DIR/index.js"
   local COMPONENT_FILE="$COMPONENT_DIR/$COMPONENT_NAME.jsx"
@@ -15,10 +16,15 @@ mkcomp() (
   createComponentFile() {
     if ! touch "$COMPONENT_FILE"; then return 1; fi
     echo "function $COMPONENT_NAME() {" &>/dev/null >>$COMPONENT_FILE
-    echo -e "\treturn (\n\t\t<p>This is a component</p>\n\t);" &>/dev/null >>$COMPONENT_FILE
+    echo -e "\treturn (\n\t\t<p>This is the $COMPONENT_NAME component</p>\n\t);" &>/dev/null >>$COMPONENT_FILE
     echo "}" &>/dev/null >>$COMPONENT_FILE
     echo -e "\nexport default $COMPONENT_NAME;" &>/dev/null >>$COMPONENT_FILE
     return 0
+  }
+
+  createDir() {
+    local TARGET_DIR="$SRC/$1"
+    [[ ! -d "$TARGET_DIR" ]] && mkdir -p "$TARGET_DIR"
   }
 
   if [[ ! -d "$CWD/src" ]]; then
@@ -31,4 +37,32 @@ mkcomp() (
   if createIndex; then echo "Barrel file created"; fi
   if createComponentFile; then echo "Component file created"; fi
 
+  case "$2" in
+  --ui)
+    createDir "ui"
+    mv "$COMPONENT_DIR" "$SRC/ui"
+    ;;
+  --context)
+    createDir "context"
+    mv "$COMPONENT_DIR" "$SRC/context"
+    ;;
+  --features)
+    createDir "features"
+    mv "$COMPONENT_DIR" "$SRC/features"
+    ;;
+  --layout)
+    createDir "layout"
+    mv "$COMPONENT_DIR" "$SRC/layout"
+    ;;
+  --pages)
+    createDir "pages"
+    mv "$COMPONENT_DIR" "$SRC/pages"
+    ;;
+  2 | 3)
+    echo 2 or 3
+    ;;
+  *)
+    echo "Unknown component directory. Keeping component in 'src' directory"
+    ;;
+  esac
 )
