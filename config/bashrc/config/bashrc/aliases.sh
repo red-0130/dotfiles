9a1c5639140f545b1d8c9b80e781accf6e58bc9c
red-0130 <100506096+red-0130@users.noreply.github.com>
2026-01-25 18:25:52 -0500

    fix(bash alias): fix env variable checking

--- config/bashrc/config/bashrc/aliases.sh
+++ config/bashrc/config/bashrc/aliases.sh
@@ -41,4 +41,5 @@ alias upgrade='sudo apt full-upgrade -y'
 # project commands
 alias personal="cd $HOME/Codespace/projects/personal"
 alias playground="cd $HOME/Codespace/projects/playground"
-[ -n "$PROJECT_WORKSPACE" ] && alias workspace="cd $PROJECT_WORKSPACE"
+! [ -z ${PROJECT_WORKSPACE+x} ] && alias workspace="cd $PROJECT_WORKSPACE"
+
