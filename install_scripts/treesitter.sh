bd8d89bcdf102df22a640e8d040df8cf7960a4c2
red-0130 <100506096+red-0130@users.noreply.github.com>
2026-05-15 04:26:38 -0400

    fix(tree-sitter installer): fix copy permission issue

--- install_scripts/treesitter.sh
+++ install_scripts/treesitter.sh
@@ -5,8 +5,9 @@ main() {
   log -i "Installing tree-sitter-cli"
   log -i "Downloading from repo..."
   curl -sL https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-linux-x64.gz |
-    gunzip >/usr/local/bin/tree-sitter && log -s "Download successful."
-  chmod +x /usr/local/bin/tree-sitter
+    gunzip >/tmp/tree-sitter && log -s "Download successful."
+  chmod +x /tmp/tree-sitter
+  sudo mv /tmp/tree-sitter /usr/local/bin/
   log -i "Finished installing tree-sitter-cli"
 }
 
