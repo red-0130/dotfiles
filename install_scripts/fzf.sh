9874f180d051da1581dae20ceeaf9639ac36a392
red-0130 <100506096+red-0130@users.noreply.github.com>
2026-05-15 04:31:41 -0400

    fix(fzf installer): fix syntax

--- install_scripts/fzf.sh
+++ install_scripts/fzf.sh
@@ -4,7 +4,7 @@ main() {
   local APP="fzf"
   log -i "Installing fzf..."
   log -i "Downloading install files from repo..."
-  [[ -d "$HOME/.fzf"]] && log -w "Removing existing installation..." &&  sudo rm -rf "$HOME/.fzf"
+  [[ -d "$HOME/.fzf" ]] && log -w "Removing existing installation..." && sudo rm -rf "$HOME/.fzf"
   if git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf; then
     log -i "Files downloaded. Starting installation now."
   else
